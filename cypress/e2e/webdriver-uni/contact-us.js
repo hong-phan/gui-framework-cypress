1a740c1217441445ff0fba897c159745e27cb2c6
Hong Phan <ptthong21676@gmail.com>
2022-11-29 17:57:12 +0700

    Update webdriver-uni/contact-us.js

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -1,25 +1,26 @@
 /// <reference types="Cypress"/>
 describe("Test Contact Us form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via Contact Us", ()=> {
-        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
-        //cy.visit("http://www.webdriveruniversity.com/")
-        //cy.get('#contact-us').click()
-        cy.get('[name="first_name"]').type("Pink")
-
-        cy.get('[name="last_name"]').type('Phan')
-        cy.get('[name="email"]').type('pink_phan@gmail.com')
-        cy.get('textarea.feedback-input').type('Learn Cypress')
-        cy.get('[type="submit"]').click()
-
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.document().should('have.property','charset').and('eq','UTF-8');
+        cy.title().should('include','WebDriver | Contact Us');
+        cy.url('').should('include', 'contactus');
+        cy.get('[name="first_name"]').type("Pink");
+        cy.get('[name="last_name"]').type('Phan');
+        cy.get('[name="email"]').type('pink_phan@gmail.com');
+        cy.get('textarea.feedback-input').type('Learn Cypress');
+        cy.get('[type="submit"]').click();
+        cy.get('h1').should('have.text', 'Thank You for your Message!');
 
     });
 
     it("Should not be able to submit a successful submission via Contact Us form as all fields are required", ()=> {
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
-        cy.get('[name="first_name"]').type("Pink")
-        cy.get('[name="last_name"]').type('Phan')
-        cy.get('[name="email"]').type('pink_phan@gmail.com')
-        cy.get('[type="submit"]').click()
+        cy.get('[name="first_name"]').type("Pink");
+        cy.get('[name="last_name"]').type('Phan');
+        cy.get('[name="email"]').type('pink_phan@gmail.com');
+        cy.get('[type="submit"]').click();
+        cy.get('body').contains('Error: all fields are required')
     })
 
 
