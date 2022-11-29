d6972c88eb2e72ac9ba0a5f1c7c0ead762d41393
Hong Phan <ptthong21676@gmail.com>
2022-11-29 17:58:10 +0700

    Updat automation-test-store/contact-us.js

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -1,13 +1,14 @@
 /// <reference types="Cypress"/>
 describe("Test Contact Us form via Automation Test Store", () => {
-    it("Should be able to submit a successful submission via Contact Us", ()=> {
+    it("Should be able to submit a successful submission via Contact Us", () => {
         cy.visit("https://automationteststore.com/");
         cy.get('[href*="contact"]').click();
         cy.get('#ContactUsFrm_first_name').type('Pink');
         cy.get('#ContactUsFrm_email').type('pink_phan@gmail.com');
+        cy.get('#ContactUsFrm_email').should('have.attr', 'name', 'email');
         cy.get('#ContactUsFrm_enquiry').type('Do you provide additional discount on bulk orders?');
         cy.get('[title="Submit"]').click();
-
+        cy.get('.mb40 > :nth-child(3)').should('have.text', 'Your enquiry has been successfully sent to the store owner!')
     });
 
 
