d2ef32331652dd15a28128b93e8e943deff9cb43
Hong Phan <ptthong21676@gmail.com>
2022-12-01 22:16:07 +0700

    Add 'then command'

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -2,7 +2,9 @@
 describe("Test Contact Us form via Automation Test Store", () => {
     it("Should be able to submit a successful submission via Contact Us", () => {
         cy.visit("https://automationteststore.com/");
-        cy.get('[href*="contact"]').click();
+        cy.get('a[href$="contact"]').click().then(function (linkText) {
+            cy.log("Clicked on link using text: " + linkText.text())
+        });
         cy.get('#ContactUsFrm_first_name').type('Pink');
         cy.get('#ContactUsFrm_email').type('pink_phan@gmail.com');
         cy.get('#ContactUsFrm_email').should('have.attr', 'name', 'email');
@@ -11,8 +13,6 @@ describe("Test Contact Us form via Automation Test Store", () => {
         cy.get('.mb40 > :nth-child(3)').should('have.text', 'Your enquiry has been successfully sent to the store owner!')
         cy.log("Test has completed");
     });
-
-
 });
 
 
