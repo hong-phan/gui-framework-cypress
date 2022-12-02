c4221ed409790f81d5bca0f9fb60e8b97ea31e29
Hong Phan <ptthong21676@gmail.com>
2022-12-02 20:06:49 +0700

    Update automation-test-store/variables-and-cy-commands.js - Variables, Promises and Nested Closures

--- cypress/e2e/automation-test-store/variables-and-cy-commands.js
+++ cypress/e2e/automation-test-store/variables-and-cy-commands.js
@@ -24,11 +24,31 @@ describe('Verify variables, cypress commands and jquery commands', () => {
         // const header = cy.get("h1 .maintext");
         // cy.log(header.text());
 
-        cy.get("h1 .maintext").then(($headerText)=>{
+        cy.get("h1 .maintext").then(($headerText) => {
             const headerText = $headerText.text();
             cy.log("Found header text: " + headerText);
             expect(headerText).is.eq('Makeup')
         })
+    });
+
+    it.only('Validate properties of the Contact Page', () => {
+        cy.visit("https://automationteststore.com/index.php?rt=content/contact");
+
+        // Used cypress commands and chaining
+        cy.contains("#ContactUsFrm", 'Contact Us Form').find("#field_11").should('contain', 'First name');
+        // JQuery Approach
+        
+        cy.contains('#ContactUsFrm', 'Contact Us Form').then(text => {
+            const firstNameText = text.find('#field_11').text();
+            expect(firstNameText).to.contain('First name');
+
+            // Embedded commands (Closure)
+            cy.get('#field_11').then(fnText => {
+                cy.log(fnText.text())
+                cy.log(fnText)
+            })
+        })
+
     });
 
 });
