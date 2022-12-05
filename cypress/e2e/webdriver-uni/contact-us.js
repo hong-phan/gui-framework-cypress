502549f7afa146acf39a279b785d372afb6fc7fd
Hong Phan <ptthong21676@gmail.com>
2022-12-05 14:35:45 +0700

    Add Handling Multiple Browser Tabs by using 'removeAttr'

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -1,7 +1,9 @@
 /// <reference types="Cypress"/>
 describe("Test Contact Us form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via Contact Us", ()=> {
-        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        // cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.visit("http://www.webdriveruniversity.com/");
+        cy.get('#contact-us').invoke('removeAttr', 'target').click();
         cy.document().should('have.property','charset').and('eq','UTF-8');
         cy.title().should('include','WebDriver | Contact Us');
         cy.url('').should('include', 'contactus');
@@ -11,19 +13,18 @@ describe("Test Contact Us form via WebdriverUni", () => {
         cy.get('textarea.feedback-input').type('Learn Cypress');
         cy.get('[type="submit"]').click();
         cy.get('h1').should('have.text', 'Thank You for your Message!');
-
     });
 
     it("Should not be able to submit a successful submission via Contact Us form as all fields are required", ()=> {
-        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        // cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.visit("http://www.webdriveruniversity.com");
+        cy.get('#contact-us').invoke('removeAttr', 'target').click();
         cy.get('[name="first_name"]').type("Pink");
         cy.get('[name="last_name"]').type('Phan');
         cy.get('[name="email"]').type('pink_phan@gmail.com');
         cy.get('[type="submit"]').click();
         cy.get('body').contains('Error: all fields are required')
     })
-
-
 });
 
 
