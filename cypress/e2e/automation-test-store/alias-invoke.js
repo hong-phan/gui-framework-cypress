9edff55c2a6fe33dd44fd19edbea6e058c66d014
Hong Phan <ptthong21676@gmail.com>
2022-12-05 14:16:07 +0700

    Update alias-invoke.js

--- cypress/e2e/automation-test-store/alias-invoke.js
+++ cypress/e2e/automation-test-store/alias-invoke.js
@@ -19,16 +19,36 @@ describe('Alias and invoke', () => {
     it.only('Calculate total of normal and sale products ', () => {
         cy.visit("https://automationteststore.com/");
         cy.get('.thumbnail').as('productThumbnail');
-        // cy.get('@productThumbnail').find('.oneprice').each(($el, index, $list) => {
-        //     cy.log($el.text());
-        // });
         cy.get('.thumbnail').find('.oneprice').invoke('text').as('itemPrice');
+        cy.get('.thumbnail').find('.pricenew').invoke('text').as('saleItemPrice');
+
+        var itemsTotal = 0;
         cy.get('@itemPrice').then($linkText => {
+            var itemPriceTotal = 0;
             var itemPrice = $linkText.split('$');
             var i;
             for(i = 0; i < itemPrice.length; i++){
                 cy.log(itemPrice[i])
+                itemPriceTotal += Number(itemPrice[i]);
             }
+            itemsTotal += itemPriceTotal;
+            cy.log("Non sale price items total: " + itemPriceTotal);
+        })
+
+        cy.get('@saleItemPrice').then($linkText => {
+            var saleItemsPrice = 0
+            var saleItemPrice = $linkText.split('$');
+            var i;
+            for(i = 0; i < saleItemPrice.length; i++){
+                cy.log(saleItemPrice[i])
+                saleItemsPrice += Number(saleItemPrice[i]);
+            } 
+            itemsTotal += saleItemsPrice;
+            cy.log("Sale price items total:  " + saleItemsPrice);           
+        })
+        .then(() => {
+            cy.log("The total price of all products: " + itemsTotal);
+            expect(itemsTotal).to.equal(625.6);
         })
     });
 });
