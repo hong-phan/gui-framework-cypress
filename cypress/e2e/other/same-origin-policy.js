205e6c2df449edc2dedf620d8467dcc9089c19d9
Hong Phan <ptthong21676@gmail.com>
2022-12-05 16:47:37 +0700

    Add 'Origin command'

--- cypress/e2e/other/same-origin-policy.js
+++ cypress/e2e/other/same-origin-policy.js
@@ -4,8 +4,23 @@ describe('Cypress web security', () => {
         cy.visit("http://www.webdriveruniversity.com/");
         cy.visit("https://automationteststore.com/");
     });
+
     it('Validate visiting two different domains via user actions', () => {
         cy.visit("http://www.webdriveruniversity.com/");
         cy.get('#automation-test-store').invoke('removeAttr', 'target').click();
+    });
+
+    it('Origin command', () => {
+        cy.origin('webdriveruniversity.com', () => {
+            cy.visit("/");
+        })
+
+        cy.origin('automationteststore.com', () => {
+            cy.visit("/");
+        })
+        
+               //Same Origin Example:
+        // cy.visit("https://www.webdriveruniversity.com/");
+        // cy.visit("https://selectors.webdriveruniversity.com/");
     });
 });
